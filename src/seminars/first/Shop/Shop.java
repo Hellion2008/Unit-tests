5bf5efa8f38163d30678576a91f07ad67d34e794
Sergey <semakin.s.v@gmail.com>
2023-12-20 13:50:00 +0300

    done task1 hw1

---
+++ src/seminars/first/Shop/Shop.java
@@ -0,0 +1,34 @@
+package seminars.first.Shop;
+
+import java.util.List;
+
+public class Shop {
+    private List<Product> products;
+
+    // Геттеры, сеттеры:
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    // Метод должен вернуть отсортированный по возрастанию по цене список продуктов
+    public List<Product> sortProductsByPrice() {
+        // Допишите реализацию метода самостоятельно
+        return null;
+    }
+
+    // Метод должен вернуть самый дорогой продукт
+    public Product getMostExpensiveProduct() {
+        // Допишите реализацию метода самостоятельно
+        Product product = null;
+        for (Product p: products){
+            if (p.getCost() > product.getCost())
+                product = p;
+        }
+        return product;
+    }
+
+}
